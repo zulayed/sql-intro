85df47a2c0b97371067950c247d359956abd1659
zulayed <ali.zulayed@kellogg.northwestern.edu>
2025-01-22 18:16:12 -0600

    Initial commit

---
+++ demo.sql
@@ -0,0 +1,8 @@
+-- Configure mode and headers columns
+-- Note: only needs to be configured once
+.mode columns
+.headers on
+
+-- Get the first and last name of every player in the database.
+
+SELECT first_name, last_name FROM players;
