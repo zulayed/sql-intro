3340be8ca621a69307b86a02f51f0e6735509b4d
Md Ali Zulayed <ali.zulayed@kellogg.northwestern.edu>
2025-01-29 18:17:20 +0000

    Updating the school table

--- school-5-insert.sql
+++ school-5-insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO students 
-    (first_name,last_name,email)
-VALUES 
-    (
-        "Md Ali","Zulayed","zulayed@gmail.com"
-      
-    )
-;
+INSERT INTO students (
+    first_name,
+    last_name,
+    email) 
+
+VALUES (
+    "Farhana","Mahfuz","dinar@gmail.com"
+)
---
+++ school-6-update.sql
@@ -0,0 +1,1 @@
+UPDATE students SET email = "farhana.m.dinar@gmail.com" WHERE id=2
